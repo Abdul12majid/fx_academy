48bdfed2ce5a731542dc47fd9b5e0efcb45cb250
Abdul12majid <yisaabdulmajid@gmail.com>
2023-09-25 16:07:12 +0100

    final commit

--- fx_class/settings.py
+++ fx_class/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'django-insecure-vl&23qiphs)422%j8u8%2j3&#9o#yfut6(r26%(yuv8=gw9e(w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
